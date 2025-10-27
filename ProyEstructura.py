9c55ce5b9b14072d7e8260a7611b592d59de4bea
Carolina Cano <ccanod@miumg.edu.gt>
2025-10-27 05:04:18 -0600

    Realiza ciclos o caminos de Euler y Hamilton.

--- ProyEstructura.py
+++ ProyEstructura.py
@@ -130,4 +130,5 @@ def descargar_csv():
     )
 if __name__ == "__main__":
     ProyGrafoGuardar.tabla_grafos()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
